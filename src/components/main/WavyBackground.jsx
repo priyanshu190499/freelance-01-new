df6770ff1579049b2ddc226b2f72c785bc93e41e
Priyanshu-Geekster <priyanshoochoudhary@gmail.com>
2025-07-06 00:57:33 +0530

    updates in hero height

--- src/components/main/WavyBackground.jsx
+++ src/components/main/WavyBackground.jsx
@@ -111,7 +111,7 @@ export function WavyBackground({
   return (
     <div
       className={cn(
-        "h-screen flex flex-col  items-center justify-end md:justify-center",
+        "h-screen md:h-[60vh] flex flex-col  items-center justify-end md:justify-center",
         containerClassName
       )}
     >
